a21bcea9f01abfc8fa9fae4d4dcccc7acd699ba9
lsh <rrgks@naver.com>
2022-04-25 18:32:56 +0900

    Refactor: processCtrl 생성 및 적용

--- app/src/apis/product/product.ctrl.js
+++ app/src/apis/product/product.ctrl.js
@@ -2,22 +2,15 @@
 
 const Product = require('../../models/Product/Product');
 
+const processCtrl = require('../../util/processCtrl');
+
 const process = {
   findAllProduct: async (req, res) => {
     const product = new Product(req);
 
     const response = await product.findAllProduct();
 
-    if (response.status < 400) {
-      return res.status(response.status).json(response);
-    }
-    if (response.status < 500) {
-      return res.status(response.status).json(response);
-    }
-
-    delete response.err;
-
-    return res.status(response.status).json(response);
+    return processCtrl(res, response);
   },
 };
 
